2e669f60d4637eb5b48f1c8e1889e06733641808
Spencer Davis <spencerdavis226@gmail.com>
2025-02-12 12:14:36 -0500

    Added capability for multiple inputs

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,23 +1,65 @@
 import { useState } from 'react';
 
 const UserForm = () => {
-  const [username, setUsername] = useState('ChickenLady');
-  const handleChange = (e) => {
-    setUsername(e.target.value);
+  const initialState = {
+    username: '',
+    email: '',
+    password: '',
   };
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((data) => ({
+      ...data,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Created User: ${username}`);
+    const { username, email, password } = formData;
+    alert(
+      `Created user, ${username} with email ${email} and password ${password}`
+    );
+    setFormData(initialState);
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      {/* Label is to identify what input is for (also for accessability). htmlFor should match input's id. */}
+      {/* Name must match exactly what you have in UserForm for things to link */}
+
+      <label htmlFor="username">Username</label>
       <input
+        id="username"
         type="text"
+        name="username"
         placeholder="Username"
-        value={username}
+        value={formData.username}
         onChange={handleChange}
       />
+
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        name="password"
+        placeholder="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+
       <button>Add me to to list!</button>
     </form>
   );
